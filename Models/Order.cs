2a2bb31bffd61e2a0d12b75e05f775d9bcf65c11
Eric <ehill111@comcast.net>
2020-10-15 13:47:54 -0400

    Added properties to Order model.

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace EnclaveGrantParkCoffeeShop.Models
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        [ForeignKey("CustomerId")]
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderInstructions { get; set; }
+        public double OrderAmountTotal { get; set; }
+        public bool OrderPending { get; set; }
+        public bool OrderFulfilled { get; set; }
     }
 }
