46127ae1d3b01f8382462cfbc20a0049bd604ad7
Eric <ehill111@comcast.net>
2020-10-15 13:15:21 -0400

    Created Customer model properties.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 namespace EnclaveGrantParkCoffeeShop.Models
 {
     public class Customer
-    {
+    {   
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+        public string CustomerUserName { get; set; }
+        public string CustomerPassword { get; set; }
+        public int RewardPointsBalance { get; set; }
     }
 }
