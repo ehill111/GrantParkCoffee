e659f7e8456347c0bcee93fe6a62cdbd7ad8a952
Eric <ehill111@comcast.net>
2020-10-15 13:54:34 -0400

    Added properties to Product model.

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,5 +7,12 @@ namespace EnclaveGrantParkCoffeeShop.Models
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string ProductType { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductFeatured { get; set; }
+        public bool ProductOnDiscountSale { get; set; }
+        public bool RewardEligible { get; set; }
     }
 }
