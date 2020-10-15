3e194846ca64c4404023790da0bea0213ea4022e
Eric <ehill111@comcast.net>
2020-10-15 14:08:58 -0400

    Added properties for RoomReservation model.

--- Models/RoomReservation.cs
+++ Models/RoomReservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace EnclaveGrantParkCoffeeShop.Models
 {
     public class RoomReservation
     {
+        public int RoomReservationId { get; set; }
+        public DateTime ReservationStartDate { get; set; }
+        public DateTime ReservationStopDate { get; set; }
+        public DateTime ReservationTimeStart { get; set; }
+        public DateTime ReservationTimeStop { get; set; }
+        public string ReservationCustomerName { get; set; }
+        [ForeignKey("CustomerId")]
+        public int CustomerId { get; set; }
     }
 }
