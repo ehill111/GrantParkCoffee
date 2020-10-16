d7cf5bd8803218cc6653bf254569e1c61d45fe29
Eric <ehill111@comcast.net>
2020-10-16 13:52:12 -0400

    1. Added identity, users, and roles. 2. Did migration and updated database.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using GrantParkCoffeeShop2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +10,7 @@ namespace GrantParkCoffeeShop2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        // public object modelBuilder;
+        public object modelBuilder;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -17,124 +18,142 @@ namespace GrantParkCoffeeShop2.Data
 
         }
 
-        public DbSet<Product> Product { get; set; }
-        public DbSet<ProductSalesHistory> ProductSalesHistory { get; set; }
-        public DbSet<ShoppingCart> ShoppingCart { get; set; }
-        public DbSet<RoomReservation> RoomReservation { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Order> Order { get; set; }
+        //public DbSet<Product> Product { get; set; }
+        //public DbSet<ProductSalesHistory> ProductSalesHistory { get; set; }
+        //public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        //public DbSet<RoomReservation> RoomReservation { get; set; }
+        //public DbSet<Customer> Customer { get; set; }
+        //public DbSet<Order> Order { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-                 builder.Entity<Product>().HasData(
-                   new Product
-                   {
-                       ProductId = 1,
-                       ProductType = "Food",
-                       ProductName = "Bacon, Egg, and Cheese Croissant",
-                       ProductPrice = 6.99,
-                       ProductFeatured = false,
-                       ProductOnDiscountSale = false,
-                       RewardEligible = true,
-                   },
-                   new Product
-                   {
-                       ProductId = 2,
-                       ProductType = "Food",
-                       ProductName = "Chicken Salad Sandwich",
-                       ProductPrice = 6.99,
-                       ProductFeatured = true,
-                       ProductOnDiscountSale = false,
-                       RewardEligible = true,
-                   },
-                    new Product
-                    {
-                        ProductId = 3,
-                        ProductType = "Food",
-                        ProductName = "Creamy Cheese Grits",
-                        ProductPrice = 2.99,
-                        ProductFeatured = false,
-                        ProductOnDiscountSale = false,
-                        RewardEligible = true,
-                    },
-                     new Product
-                     {
-                         ProductId = 4,
-                         ProductType = "Food",
-                         ProductName = "Spinach Salad",
-                         ProductPrice = 6.99,
-                         ProductFeatured = false,
-                         ProductOnDiscountSale = false,
-                         RewardEligible = true,
-                     },
-                      new Product
-                      {
-                          ProductId = 5,
-                          ProductType = "Dessert",
-                          ProductName = "Banana Nut Bread",
-                          ProductPrice = 2.99,
-                          ProductFeatured = false,
-                          ProductOnDiscountSale = false,
-                          RewardEligible = true,
-                      },
-                       new Product
-                       {
-                           ProductId = 6,
-                           ProductType = "Dessert",
-                           ProductName = "Chocolate Chip Skone",
-                           ProductPrice = 2.99,
-                           ProductFeatured = false,
-                           ProductOnDiscountSale = false,
-                           RewardEligible = true,
-                       },
-                        new Product
-                        {
-                            ProductId = 7,
-                            ProductType = "Dessert",
-                            ProductName = "Blueberry Muffin",
-                            ProductPrice = 2.99,
-                            ProductFeatured = false,
-                            ProductOnDiscountSale = false,
-                            RewardEligible = true,
-                        },
-                         new Product
-                         {
-                             ProductId = 8,
-                             ProductType = "Beverage",
-                             ProductName = "Coffee",
-                             ProductPrice = 2.49,
-                             ProductFeatured = false,
-                             ProductOnDiscountSale = false,
-                             RewardEligible = true,
-                         },
-                         new Product
-                         {
-                             ProductId = 9,
-                             ProductType = "Beverage",
-                             ProductName = "Juice",
-                             ProductPrice = 2.99,
-                             ProductFeatured = false,
-                             ProductOnDiscountSale = false,
-                             RewardEligible = true,
-                         },
-                          new Product
-                          {
-                              ProductId = 10,
-                              ProductType = "Beverage",
-                              ProductName = "Water",
-                              ProductPrice = 1.49,
-                              ProductFeatured = false,
-                              ProductOnDiscountSale = false,
-                              RewardEligible = false
-                          }
-                          );
+            base.OnModelCreating(builder);
 
-                   }
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole
+                    {
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    }
+                );
+        }
+    }
+
+}     
+    
+
+
+            //builder.Entity<Product>().HasData(
+                 //  new Product
+
+    //               {
+    //                   ProductId = 1,
+    //                   ProductType = "Food",
+    //                   ProductName = "Bacon, Egg, and Cheese Croissant",
+    //                   ProductPrice = 6.99,
+    //                   ProductFeatured = false,
+    //                   ProductOnDiscountSale = false,
+    //                   RewardEligible = true,
+    //               },
+    //               new Product
+    //               {
+    //                   ProductId = 2,
+    //                   ProductType = "Food",
+    //                   ProductName = "Chicken Salad Sandwich",
+    //                   ProductPrice = 6.99,
+    //                   ProductFeatured = true,
+    //                   ProductOnDiscountSale = false,
+    //                   RewardEligible = true,
+    //               },
+    //                new Product
+    //                {
+    //                    ProductId = 3,
+    //                    ProductType = "Food",
+    //                    ProductName = "Creamy Cheese Grits",
+    //                    ProductPrice = 2.99,
+    //                    ProductFeatured = false,
+    //                    ProductOnDiscountSale = false,
+    //                    RewardEligible = true,
+    //                },
+    //                 new Product
+    //                 {
+    //                     ProductId = 4,
+    //                     ProductType = "Food",
+    //                     ProductName = "Spinach Salad",
+    //                     ProductPrice = 6.99,
+    //                     ProductFeatured = false,
+    //                     ProductOnDiscountSale = false,
+    //                     RewardEligible = true,
+    //                 },
+    //                  new Product
+    //                  {
+    //                      ProductId = 5,
+    //                      ProductType = "Dessert",
+    //                      ProductName = "Banana Nut Bread",
+    //                      ProductPrice = 2.99,
+    //                      ProductFeatured = false,
+    //                      ProductOnDiscountSale = false,
+    //                      RewardEligible = true,
+    //                  },
+    //                   new Product
+    //                   {
+    //                       ProductId = 6,
+    //                       ProductType = "Dessert",
+    //                       ProductName = "Chocolate Chip Skone",
+    //                       ProductPrice = 2.99,
+    //                       ProductFeatured = false,
+    //                       ProductOnDiscountSale = false,
+    //                       RewardEligible = true,
+    //                   },
+    //                    new Product
+    //                    {
+    //                        ProductId = 7,
+    //                        ProductType = "Dessert",
+    //                        ProductName = "Blueberry Muffin",
+    //                        ProductPrice = 2.99,
+    //                        ProductFeatured = false,
+    //                        ProductOnDiscountSale = false,
+    //                        RewardEligible = true,
+    //                    },
+    //                     new Product
+    //                     {
+    //                         ProductId = 8,
+    //                         ProductType = "Beverage",
+    //                         ProductName = "Coffee",
+    //                         ProductPrice = 2.49,
+    //                         ProductFeatured = false,
+    //                         ProductOnDiscountSale = false,
+    //                         RewardEligible = true,
+    //                     },
+    //                     new Product
+    //                     {
+    //                         ProductId = 9,
+    //                         ProductType = "Beverage",
+    //                         ProductName = "Juice",
+    //                         ProductPrice = 2.99,
+    //                         ProductFeatured = false,
+    //                         ProductOnDiscountSale = false,
+    //                         RewardEligible = true,
+    //                     },
+    //                      new Product
+    //                      {
+    //                          ProductId = 10,
+    //                          ProductType = "Beverage",
+    //                          ProductName = "Water",
+    //                          ProductPrice = 1.49,
+    //                          ProductFeatured = false,
+    //                          ProductOnDiscountSale = false,
+    //                          RewardEligible = false
+    //                      }
+    //                      );
+
+    //               }
 
             
 
-        }
-    }
+    //    }
+    //}
 
     
     
--- obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\ehill\Documents\Eric's Present Files\SoftwareAndWebDevelopment\devCodeCamp\GradedAssignments\SoloCapstone\GrantPark\EnclaveGrantParkCoffeeShop\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\ehill\Documents\Eric's Present Files\SoftwareAndWebDevelopment\devCodeCamp\GradedAssignments\SoloCapstone\GrantPark\EnclaveGrantParkCoffeeShop\Areas\Identity\Pages\_ViewImports.cshtml"
+using GrantParkCoffeeShop2.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\ehill\Documents\Eric's Present Files\SoftwareAndWebDevelopment\devCodeCamp\GradedAssignments\SoloCapstone\GrantPark\EnclaveGrantParkCoffeeShop\Areas\Identity\Pages\_ViewImports.cshtml"
+using GrantParkCoffeeShop2.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"630d660b7217f2eaf3554c22d39e20cf405d8d20", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
