6a6a7b834c135492a3d7c50e9cd6b0d9c3768b18
Eric <ehill111@comcast.net>
2020-10-17 12:39:27 -0400

    1. Added ViewsOfShop folder. 2. Created ProductModel.cs for view. 3. Added method to search all products.

---
+++ ViewsOfShop/ProductModel.cs
@@ -0,0 +1,40 @@
+﻿using GrantParkCoffeeShop2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrantParkCoffeeShop2.ViewsOfShop
+{
+    public class ProductModel
+    {
+        public List<Product> _products { get; set; }
+        public List<Product> allProducts()
+        {
+            _products = new List<Product>();
+            return _products;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
