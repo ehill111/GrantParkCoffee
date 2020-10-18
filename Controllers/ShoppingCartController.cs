1c9b81500927fe75310242c3e925f17421996954
Eric <ehill111@comcast.net>
2020-10-18 15:03:41 -0400

    Added sum method to shopping cart.

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrantParkCoffeeShop2.Helpers;
+using GrantParkCoffeeShop2.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GrantParkCoffeeShop2.Controllers
 {
-    public class ShoppingCart : Controller
+    public class ShoppingCartController : Controller
     {
         public IActionResult Index()
         {
+            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            ViewBag.cart = cart;
+            ViewBag.total = cart.Sum(item => item.Product.ProductPrice * item.Quantity);
+
             return View();
         }
     }
