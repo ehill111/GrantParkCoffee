3eb64c66ac4f4522d65cdaf64c2e4caa3a457e4e
Eric <ehill111@comcast.net>
2020-10-20 09:04:41 -0400

    Added ProductContext class and listed DbSets.

--- Models/ProductDatabaseInitializer.cs
+++ Models/ProductDatabaseInitializer.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace NewGrantParkCoffeeShop.Models
 {
-    public class ProductDatabaseInitializer
+    public class ProductDatabaseInitializer : DropCreateDatabaseIfModelChanges<ProductContext>
     {
+        protected override void Seed(ProductContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetProducts().ForEach(p => context.Products.Add(p));
+        }
 
-    }
-}
+        private static List<Category> GetCategories()
+        {
+            var categories = new List<Category> {
+                new Category
+                {
+                    CategoryID = 1,
+                    CategoryName = "Food"
+                },
+                new Category
+                {
+                    CategoryID = 2,
+                    CategoryName = "Dessert"
+                },
+                new Category
+                {
+                    CategoryID = 3,
+                    CategoryName = "Beverage"
+                },
+            };
+
+            return categories;
+        }
+
