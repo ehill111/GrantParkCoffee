1a492cd35c21404b9ce10972cc4493547a5b7f38
Eric <ehill111@comcast.net>
2020-10-20 10:31:54 -0400

    Listed data in ProductDatabaseInitializer to be seeded in database.

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.Entity;
+using NewGrantParkCoffeeShop.Models;
 
 namespace NewGrantParkCoffeeShop
 {
@@ -16,6 +18,9 @@ namespace NewGrantParkCoffeeShop
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Initalize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
         }
     }
 }
